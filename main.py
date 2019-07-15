2e401f29e0e0c7862d800c3b5c441731828ef00e
CDavis <CDavis@dstsytems.com>
2019-07-15 11:43:32 -0500

    Added player sprite and display settings

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import pygame
 
-game_display = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('RPyG')
-clock = pygame.time.Clock()
 
 pygame.init()
 
+DISPLAY_WIDTH = 800
+DISPLAY_HEIGHT = 600
+game_display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+pygame.display.set_caption('RPyG')
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+PLAYER_SPEED = 10
+
+clock = pygame.time.Clock()
+player_img = pygame.image.load('assets/player.png')
 dead = False
+
+
+def player(x, y):
+    game_display.blit(player_img, (x, y))
+
+
 while not dead:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             dead = True
-
+            
         print(event)
 
+    game_display.fill(BLACK)
+    player(0.5 * DISPLAY_WIDTH, 0.5 * DISPLAY_HEIGHT)
     pygame.display.update()
     clock.tick(60)
 
