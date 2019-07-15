1f896018869fff7ba1c1b2d25be60a21f84d3868
Camden Davis <camdendavis99@gmail.com>
2019-07-15 12:01:12 -0500

    Added movement and updated player sprite

--- main.py
+++ main.py
@@ -21,16 +21,40 @@ def player(x, y):
     game_display.blit(player_img, (x, y))
 
 
+x = 0.5 * DISPLAY_WIDTH
+y = 0.5 * DISPLAY_HEIGHT
+v_x = 0
+v_y = 0
+
 while not dead:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             dead = True
-            
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                v_x = -PLAYER_SPEED
+            elif event.key == pygame.K_RIGHT:
+                v_x = PLAYER_SPEED
+
+            elif event.key == pygame.K_UP:
+                v_y = -PLAYER_SPEED
+            elif event.key == pygame.K_DOWN:
+                v_y = PLAYER_SPEED
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                v_x = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                v_y = 0
+
         print(event)
 
+    x += v_x
+    y += v_y
+
     game_display.fill(BLACK)
-    player(0.5 * DISPLAY_WIDTH, 0.5 * DISPLAY_HEIGHT)
+    player(x, y)
     pygame.display.update()
     clock.tick(60)
 
